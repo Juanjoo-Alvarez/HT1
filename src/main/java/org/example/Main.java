4fb82c4ceb8304d17047334bc1c31e4df342e8f8
Julio-orellana <84369228+Julio-orellana@users.noreply.github.com>
2025-01-22 13:00:10 -0600

    Testing objects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,57 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Blender b = new Blender();
+        byte[] choice = options(sc, b);
+        while (choice[1] != 0) {
+            switch (choice[1]) {
+                case 1:
+                    b.switchPowerStatus();
+                    break;
+                case 2:
+                    b.increaseSpeed();
+                    break;
+                case 3:
+                    b.decreaseSpeed();
+                    break;
+                case 4:
+                    System.out.println("Enter the product you want to fill the blender with: ");
+                    String prod = sc.next();
+                    System.out.println("Enter the quantity in ml: ");
+                    float ml = sc.nextFloat();
+                    b.fillBlender(prod, ml);
+                    break;
+                case 5:
+                    b.emptyBlender();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+            choice = options(sc, b);
         }
     }
+
+    public static byte[] options(Scanner sc, Blender b) {
+        if (b.checkPowerStatus() == 0 || b.checkPowerStatus() == 2) {
+            System.out.println("0. Exit");
+            System.out.println("1. Switch power status");
+            System.out.println("Enter your choice: ");
+            return new byte[]{b.checkPowerStatus(), sc.nextByte()};
+        } else {
+            System.out.println("0. Exit");
+            System.out.println("1. Switch power status");
+            System.out.println("2. Increase speed");
+            System.out.println("3. Decrease speed");
+            System.out.println("4. Fill blender");
+            System.out.println("5. Empty blender");
+            System.out.println("Enter your choice: ");
+            return new byte[]{b.checkPowerStatus(), sc.nextByte()};
+        }
+    }
+
 }
