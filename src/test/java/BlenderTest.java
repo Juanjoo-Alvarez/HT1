dbad47e1fb59ab762a4faf4a341ec7151382a9f5
Juanjoo-Alvarez <riv24856@uvg.edu.gt>
2025-01-22 12:18:07 -0600

    Agregando pruebas unitarias

---
+++ src/test/java/BlenderTest.java
@@ -0,0 +1,63 @@
+package src.test.java;
+
+
+public class BlenderTest {
+    private Blender blender;
+
+    @BeforeEach
+    public void setUp(){
+        blender = new Blender();
+    }
+
+    @Test
+    public void testIncreaseSpeed(){
+        blender.switchPowerStatus(); 
+        blender.fillBlender("Fruit", 500);
+
+        blender.increaseSpeed();
+        assertEquals(1, blender.checkSpeed());
+
+        blender.increaseSpeed();
+        assertEquals(2, blender.checkSpeed());
+
+        // Prueba para no exceder la velocidad máxima
+        for (int i = 0; i < 3; i++) {
+            blender.increaseSpeed();
+        }
+        assertEquals(10, blender.checkSpeed());
+    }
+
+    @Test
+    public void testDecreaseSpeed() {
+        blender.switchPowerStatus(); // Enciende la licuadora
+        blender.fillBlender("Fruit", 500); // Llena la licuadora
+
+        blender.increaseSpeed();
+        blender.increaseSpeed();
+        assertEquals(2, blender.checkSpeed());
+
+        blender.decreaseSpeed();
+        assertEquals(1, blender.checkSpeed());
+
+        blender.decreaseSpeed();
+        assertEquals(0, blender.checkSpeed());
+
+        // Prueba para no bajar de la velocidad mínima
+        blender.decreaseSpeed();
+        assertEquals(0, blender.checkSpeed());
+    }
+
+    @Test
+    public void testCheckSpeed() {
+        assertEquals(0, blender.checkSpeed()); // Velocidad inicial
+
+        blender.switchPowerStatus(); // Enciende la licuadora
+        blender.fillBlender("Fruit", 500); // Llena la licuadora
+
+        blender.increaseSpeed();
+        assertEquals(1, blender.checkSpeed());
+
+        blender.increaseSpeed();
+        assertEquals(2, blender.checkSpeed());
+    }
+}
